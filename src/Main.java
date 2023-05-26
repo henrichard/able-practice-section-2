67b0d4d65bbbb4fe7d27f7b977ba4ebaa1b553f4
henrichard <glosskraken@gmail.com>
2023-05-26 14:21:13 +0500

    init: start project

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        mainMethodShowInfo();
+        mainMethodShowMyInfo();
+    }
+
+    private static void mainMethodShowInfo() {
+
+        System.out.println("I've made a new change.");
+    }
+
+    private static void mainMethodShowMyInfo() {
+
+        System.out.println("I've made completely NEW changes to this project.");
+    }
+}
+
