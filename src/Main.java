c3fc71de9375081af477e4a1ab64f1282c3868ce
henrichard <glosskraken@gmail.com>
2023-05-27 10:24:28 +0500

    feat: add new branch

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         mainMethodShowInfo();
-        mainMethodShowMyInfo();
     }
 
     private static void mainMethodShowInfo() {
 
-        System.out.println("I've made a new change.");
+        System.out.println("DELETE me");
     }
 
-    private static void mainMethodShowMyInfo() {
-
-        System.out.println("I've made completely NEW changes to this project.");
-    }
 }
 
